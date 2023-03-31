0e2c2933bc23acffd79a233711c2437b8ba7bffb
UTKARSH SINGH <utkarshsinghus@gmail.com>
2023-04-01 02:04:51 +0530

    setup static file access for layouts and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 module.exports.profile=function(req,res){
     return res.render('profile',{
+        title:'profile page',
         p_name:'utkarsh'
     });
 }
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express=require('express');
+const expressLayouts=require('express-ejs-layouts');
 const port=8000;
 const app=express();
 
+//tell the app to use layouts done before rendering any views
+app.use(expressLayouts);
+//set the statics 
+app.use(express.static('./assets'));
+//extract style and script from subpages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express router defined in routes/index.js 
 //middleware
 app.use('/',require('./routes'));
