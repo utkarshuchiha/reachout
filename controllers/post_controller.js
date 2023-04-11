8ca7331fabc9e05b49c23c2708071b67a356f0db
UTKARSH SINGH <utkarshsinghus@gmail.com>
2023-04-11 14:17:33 +0530

    added post controller route and post form

---
+++ controllers/post_controller.js
@@ -0,0 +1,15 @@
+const Post=require('../models/post');
+
+module.exports.create=function(req,res){
+    Post.create({
+        content:req.body.content,
+        user:req.user._id
+    },(err,post)=>{
+        if(err){
+            console.log("error in creating a post in db"+err);
+            return;
+        }
+        return res.redirect('back');
+
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const homeController=require('../controllers/home_controller');
 //http request for home use different function name for different action 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./post'));
 //for any further routes access from here
 //router.use('/routeName',require('./routerfile));
 
