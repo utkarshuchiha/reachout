5576710c33e04ce225a6aaefcc00e82ebb579402
UTKARSH SINGH <utkarshsinghus@gmail.com>
2023-04-13 00:16:09 +0530

    comment delete added

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,3 +19,22 @@ module.exports.create=function(req,res){
     }
    });
 }
+
+module.exports.destroy=function(req,res){
+    Comment.findById(req.params.id).populate('post').exec(function(err,comment){  
+        
+        if(comment.user==req.user.id  ||comment.post.user==req.user.id){
+            
+            let postId=comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+                return res.redirect('back');
+            });
+             
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/post.js
+++ routes/post.js
@@ -3,4 +3,6 @@ const router=express.Router();
 const passport=require('passport');
 const postController=require('../controllers/post_controller');
 router.post('/create',passport.checkAuthentication,postController.create);
+//currently using a tag to make change afterwards will use js ajax then will change to delete or other request
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 module.exports=router;
